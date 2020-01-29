93840ff17b35e8f1290374b2ecf69f198d795d9b
Thomas BAGREL <tomsb07@gmail.com>
2020-01-29 15:19:43 +0100

    add public interface with pub use

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,11 @@ pub mod htpasswd_database;
 pub mod user_control_policy;
 
 mod auth_data;
+
+pub use auth_control::{
+    AuthControl,
+    AuthResult
+};
+pub use error::Error;
+pub use htpasswd_database::HtpasswdDatabase;
+pub use user_control_policy::UserControlPolicy;
